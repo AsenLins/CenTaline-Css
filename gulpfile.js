93527834c5a3e568f66631a8b4bdf2943df5d0f5
CHZA林根森-Kim <mzh1708587@centaline.com.cn>
2017-09-06 18:30:33 +0800

    添加部分样式

--- gulpfile.js
+++ gulpfile.js
@@ -1,8 +1,21 @@
 var gulp=require("gulp"),
     less=require("gulp-less");
 
-gulp.task("default",function(){
+gulp.task("PluginLess",function(){
+
+  gulp.src(['src/Plugin/*.less'])
+  .pipe(less())
+  .pipe(gulp.dest("dist/Plugin"));
+
+});
+
+gulp.task("Ct_CmShopPage",function(){
+  gulp.src(['Ct_CmShopPage/src/*.less'])
+  .pipe(less())
+  .pipe(gulp.dest("Ct_CmShopPage/css"));
+});
 
 
-
+gulp.task("WatchLess",function(){
+  gulp.watch("/*.less",['PluginLess','Ct_CmShopPage'])
 });
